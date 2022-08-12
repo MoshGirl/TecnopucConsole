a3e5c72390f004d91d01f4e0ad3e2739ea61087c
MoshGirl <you@example.com>
2022-08-11 20:15:37 -0400

    Adicionar arquivos de projeto.

---
+++ TecnopucConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TecnopucConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
